188271a6f53ead53a95d3cc61f8ff60baf1275bf
Hiro6634 <hiro.suyama@gmail.com>
2023-01-19 17:34:06 -0300

    Created DID Document

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { KMSClient } from "@extrimian/kms-client";
-import { LANG } from "@extrimian/kms-core";
+import { LANG, Suite } from "@extrimian/kms-core";
 import { SecureStorage } from "./secure-storage";
+import { Did } from "@extrimian/did-registry";
+import { AssertionMethodPurpose, KeyAgreementPurpose } from "@extrimian/did-core";
 
 const index = async () => {
     // Create DID
@@ -10,15 +12,39 @@ const index = async () => {
         storage: new SecureStorage()
     });
 
-    // Update DID Document
+    // Update DID Document Key
+    const updateKey = await kms.create(Suite.ES256k);
+
     // Generate Recovery Key
+    const recoveryKey = await kms.create(Suite.ES256k);
 
     // Generate DIDComm Key 
+    const didComm = await kms.create(Suite.DIDComm);
 
     // Generate BBS Key
+    const bbsbls = kms.create(Suite.Bbsbls2020);
+
+    const didService = new Did();
 
     // Create DID Document
-    // Claves DIDComm y EBS
+    const longDID = await didService.createDID({
+        updateKeys: [updateKey.publicKeyJWK],
+        recoveryKeys: [recoveryKey.publicKeyJWK],
+        verificationMethods: [{
+            //Claves DIDComm
+            id: "didComm",
+            type: "X25519KeyAgreemenKey2019",
+            publicKeyJwk: didComm.publicKeyJWK,
+            purpose: [new KeyAgreementPurpose()]
+        }, {
+            // Clave BBS
+            id: "bbsbls",
+            type: "Bls12381G1Key2020",
+            publicKeyJwk: (await bbsbls).publicKeyJWK,
+            purpose: [new AssertionMethodPurpose()]
+        }]
+    });
+    
 }
 
 index();
