2dffc89925e4f8a14c125630d1617317d4a5b2b2
Hiro6634 <hiro.suyama@gmail.com>
2023-01-19 14:40:00 -0300

    Instanciado el KMS y el SecureStorage

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
+import { KMSClient } from "@extrimian/kms-client";
+import { LANG } from "@extrimian/kms-core";
+import { SecureStorage } from "./secure-storage";
 
 const index = async () => {
-    
+    // Create DID
+
+    const kms = new KMSClient({
+        lang: LANG.en,
+        storage: new SecureStorage()
+    });
+
+    // Update DID Document
+    // Generate Recovery Key
+
+    // Generate DIDComm Key 
+
+    // Generate BBS Key
+
+    // Create DID Document
+    // Claves DIDComm y EBS
 }
 
 index();
